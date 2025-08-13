a44c0c9bf0b2e07ad6e83ae6f0119dd1811f99ae
Christopher Stadler <christopher.stadler@guavestudios.com>
2025-08-13 19:08:34 +0200

    feat: major contao 5+ update

--- src/GuaveFormSaveBundle.php
+++ src/GuaveFormSaveBundle.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace Guave\FormSaveBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Guave\FormSaveBundle\Config\Config;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class GuaveFormSaveBundle extends Bundle
+class GuaveFormSaveBundle extends AbstractBundle
 {
-    public function getPath(): string
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        return \dirname(__DIR__);
+        $container->import('../config/services.yaml');
+
+        $builder->getDefinition(Config::class)->setArgument(0, $config);
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('quote')->defaultValue('"')->end()
+            ->scalarNode('separator')->defaultValue(',')->end()
+            ->end()
+        ;
     }
 }
